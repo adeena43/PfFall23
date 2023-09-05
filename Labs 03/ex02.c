3daa2e112e5180829bde5d49082f82c86825545a
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-05 05:22:10 -0800

    Update ex02.c

--- Labs 03/ex02.c
+++ Labs 03/ex02.c
@@ -2,15 +2,14 @@
 int main()
 {	
 	
-	float length;
-	float breadth;
+	float radius;
+	float pi = 3.142;
 	float height;
 	printf("Calculating the volume of a container\n");
-	printf("\nEnter the length:");
-	scanf("%f",&length);
-	printf("\nEnter the breadth:");
-	scanf("%f",&breadth);
-	printf("Enter the height:");
+	printf("\nEnter the radius:");
+	scanf("%f",&radius);
+	printf("\nEnter the height:");
 	scanf("%f",&height);
-	printf("The volume of the container is:%f",length*breadth*height);
+	printf("\nEnter the breadth:");
+	printf("The volume of the container is:%f",pi*radius*radius*height);
 }
