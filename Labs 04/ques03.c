788c2926254f2eeaf0f9d6788ab3932e4357ac97
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-12 05:20:55 -0800

    Update ques03.c

--- Labs 04/ques03.c
+++ Labs 04/ques03.c
@@ -10,31 +10,21 @@
 
 int main()
 {	
-	int size = arr[j]
-	int arr[j];
-	int x =1;
-	int y = 1;
-	int sum = 0;
-	int i;
-	int j;
-	int k;
-	int sum=0;
-	printf("Enter an integer:");
-	scanf("%d",&i);
-	printf("\nEnter the number of integers you want the sum of squares of:");
-	scanf("%d",&j);
-	printf("\nEnter the integers:")
-	while(x<=j)
+	
+	int i =0;  //loop counter variable
+	int nums:  //stores input
+	int sum = 0;  //this variable will store the sum of the squares of inputted numbers
+	int n; //this variable will take numbers as input from the user
+	printf("Enter the number of integers of which you want the sum of squares");
+	
+	while(i<n)  //loop terminates when i become greater than n
 	{
-		scanf("%d",&arr[i]);
+		nums = 0;
+		printf("\nEnter the number %d: ",i);
+		scanf("%d",&nums);
+		sum = sum +(nums*nums);
 		i = i+1;
-	}
+	}	//end while
+	print("\nSum is:%d", sum);  //sum printed
 
-	while(j<=size-1)
-		{
-
-			sum = sum + arr[i*i];
-			j = j+1;
-		}
-	print("sum=%d",sum);
 } //end main
