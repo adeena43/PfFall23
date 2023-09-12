da88703b7e9a60d16503ece111b82d90f6b30f9d
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-12 10:50:26 +0500

    Create ques01.c

---
+++ Labs 04/ques01.c
@@ -0,0 +1,26 @@
+/*
+*Programmer:Adina
+*Date:12Sept2023
+*Description:This program prints a pyramid of stars for n numbers.
+
+//--included files--//
+#include<stdio.h>
+
+//--Global variables--//
+
+int main()
+{
+	int i=1; //a counter variable for while loop1
+	int j=1; //a counter variable for while loop2
+	while(i<=n) //this loop generates a new lines after printing one row of stars
+	{
+		printf("\n");
+		while(j<=i)
+		{
+			printf("*"); //this loop generates a new line afer every row of stars
+			j=j+1;
+		}
+		
+		
+	}
+} //end main
