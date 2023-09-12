f629f81248612094c02f97b39f37a1cdb633369d
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-12 08:02:34 -0800

    Update ques07.c

--- Labs 04/ques07.c
+++ Labs 04/ques07.c
@@ -11,11 +11,13 @@
 
 int main()
 {
+	
 	int n;
 	char letter; 
   char c[10] = {0}; //declaring a character array for inputs
   int i = 0;
   printf("enter the number of letters you want to enter (<10): ");
+	int x = n;
 	scanf("%d",&n);
   while(i<n) //entering elements to our array
   {
@@ -28,10 +30,10 @@ int main()
   i = 0;
   while(i<n/2) //checks if the elements of the array form a palindrome or not
   {
-  	if(c[i]==c[n-1])
+  	if(c[i]==c[x-1])
   	{
 		i = i+1;
-		n = n-1;
+		x = n-1;
 		
 	  }
 	  
