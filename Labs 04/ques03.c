0fcd40a50ef45744f5a6d4bf480d6f9723d629ed
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-12 10:52:32 +0500

    Create ques03.c

---
+++ Labs 04/ques03.c
@@ -0,0 +1,40 @@
+/*
+*Programmer:Adina
+*Date:12Sept2023
+*Description:This program prints the sum of the squares of  numbers inputted by the user.
+
+//--included files--//
+#include<stdio.h>
+
+//--Global variables--//
+
+int main()
+{	
+	int size = arr[j]
+	int arr[j];
+	int x =1;
+	int y = 1;
+	int sum = 0;
+	int i;
+	int j;
+	int k;
+	int sum=0;
+	printf("Enter an integer:");
+	scanf("%d",&i);
+	printf("\nEnter the number of integers you want the sum of squares of:");
+	scanf("%d",&j);
+	printf("\nEnter the integers:")
+	while(x<=j)
+	{
+		scanf("%d",&arr[i]);
+		i = i+1;
+	}
+
+	while(j<=size-1)
+		{
+
+			sum = sum + arr[i*i];
+			j = j+1;
+		}
+	print("sum=%d",sum);
+} //end main
