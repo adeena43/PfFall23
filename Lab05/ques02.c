0c48a3b70032925c7225d99fdc3dcde3a854374d
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-19 09:21:44 +0500

    Update ques02.c

--- Lab05/ques02.c
+++ Lab05/ques02.c
@@ -8,15 +8,15 @@ int main()
 	printf("\nEnter hours:");
 	scanf("%d",&minutes);
 	printf("\nYour time is: %d:%d", hours, minutes);
-	if((hours>=5)||(hours<12))
+	if((hours>=5)&&(hours<12))
 	{
 		printf("\nGood Morning");
 	}
-	else if((hours>=12)||(hours<=18))
+	else if((hours>=12)&&(hours<=18))
 		{
 			printf("\nGood Evening");
 		}
-		else if((hours>18)||(hours<=24))
+		else if((hours>18)&&(hours<=24))
 			{
 				printf("\nGood night");
 			}
