1fa6c9ae9da8349f5b707518fcfd112c67923165
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-19 06:55:31 -0800

    Update ques06.c

--- Labs 04/ques06.c
+++ Labs 04/ques06.c
@@ -12,37 +12,56 @@ int main()
 {
 	 
   int n;  
-  
+  int i,j, k;
   printf("Enter number of rows: ");  
   scanf("%d",&n);  
   
-  for(int i=1; i<=n; i++)  
-  {  
-     for(int j=i; j<=n; j++)  
-     {  
-       printf(" ");  
-     }  
-     for(int k=1; k<=2*i-1; k++)  
-     {  
-       if(k==1 || k==(2*i-1)) printf("*");  
-       else printf(" ");  
-     }  
-     printf("\n");  
-  }  
   
-  for(int i=n-1; i>=1; i--)  
-  {  
-     for(int j=n; j>=i; j--)  
-     {  
-       printf(" ");  
-     }  
-     for(int k=1; k<=2*i-1; k++)  
-     {  
-       if(k==1 || k==2*i-1) printf("*");  
-       else printf(" ");  
-     }  
-     printf("\n");  
-  }  
+	i = 1;
+	while(i<=n)  // this loop is to generate a enw line after completing one row
+	{
+		j = i;
+		while(j<=n)    //this loop prints the empty spaces
+		{
+			printf(" ");
+			j = j+1;
+		}
+		k = 1;       
+	
+		while(k<=(2*i)-1)     //prints the upper half boundary of diamond
+		{
+			if(k==2||k==(2*i)-1)
+			printf("*");
+			else
+			printf(" ");
+			k = k+1;
+		}
+	
+		printf("\n");
+		i = i+1;
+	}
+   	//Printing the lower half of the diamond
+	i = n-1;
+	while(i>=1)    //genrating new line
+	{
+		j = n;
+		while(j>=i)  //printing empty spaces out and inside of the diamond
+		{
+			printf(" ");
+			j = j-1;
+		}
+		k = 1;
+		while(k<=(2*i)-1)
+		{
+			if(k==1||k==(2*i)-1)    //printing the stars for lower half boundary of the diamond
+			printf("*");
+			else
+			printf(" ");
+		}
+	
+		printf("\n");
+		i = i+1;
+	}
   
   return 0;  
 }  
