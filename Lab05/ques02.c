91795407bd5599426a90465d99cf22fff54b24cc
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-19 09:17:42 +0500

    Create ques02.c

---
+++ Lab05/ques02.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int hours;
+	int minutes;
+	printf("\nEnter hours:");
+	scanf("%d",&hours);
+	printf("\nEnter hours:");
+	scanf("%d",&minutes);
+	printf("\nYour time is: %d:%d", hours, minutes);
+	if((hours>=5)||(hours<12))
+	{
+		printf("\nGood Morning");
+	}
+	else if((hours>=12)||(hours<=18))
+		{
+			printf("\nGood Evening");
+		}
+		else if((hours>18)||(hours<=24))
+			{
+				printf("\nGood night");
+			}
+			else
+				{
+					printf("\nInvalid time!");
+				}
+}
