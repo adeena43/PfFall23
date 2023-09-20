ba7756f779c4aa7d02f0582a682af7737ddf4b0b
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-20 07:11:27 -0800

    Create ques07.c

---
+++ Labs/L05/ques07.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+	float SalesPrice, amount;
+	printf("Please enter your amount of shopping: ");
+	scanf("%f",&amount);
+	if(amount>6000)
+	{
+		SalesPrice=amount-(amount*0.5);
+		printf("\nYour discounted price is:%f",SalesPrice);	
+	}
+	else if((amount>4000)&&(amount<=6000))
+	{	
+		SalesPrice=amount-(amount*0.3);
+		printf("\nYour discounted price is:%f",SalesPrice);
+	}
+		else if((amount>2000)&&(amount<4000))
+		{	
+			SalesPrice=amount-(amount*0.2);
+			printf("\nYour discounted price is:%f",SalesPrice);
+		}
+			else if(amount>1999)
+			{
+				SalesPrice=amount-(amount*0.5);
+				printf("\nYour discounted price is:%f",SalesPrice);
+			}
+				else
+				{
+					printf("\nYour price is:%f", amount);
+				}
+}		//end main()
