cfc3544087aab5db5290b3699fe4b074d8bb6cc3
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-20 07:21:10 -0800

    Create ques11.c

---
+++ Labs/L05/ques11.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+	int higherBits, lowerBits , bits8;
+	printf("Enter an 8-bit number:");
+	scanf("%d",&bits8);
+
+    if (bits8 >= 0 && bits8 <= 255) {
+        // Check if at least two digits are present
+        if (bits8 >= 10) {
+            int lowerBits = bits8 % 10000;       // Get the lower 4 bits
+            int higherBits = bits8 / 10000;     // Get the higher 4 bits
+
+            // Exchange the higher and lower 4 bits
+            int swappedNum = (lowerBits * 10000) + higherBits;
+
+            printf("Original number: %d\n", bits8);
+            printf("Number after swapping: %d\n", swappedNum);
+        } else {
+            printf("At least two digits are not present in the entered number.\n");
+        }
+    } else {
+        printf("Invalid input. Please enter an 8-bit number (0-255).\n");
+    }
+
+    return 0;
+
+}
