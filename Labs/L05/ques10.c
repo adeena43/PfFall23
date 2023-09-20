af7a438796557a32d165a65503f334134e6135e7
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-20 07:15:01 -0800

    Create ques10.c

---
+++ Labs/L05/ques10.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>		//including maths header file to use maths functions
+
+int main()
+{
+	float a, b, c, x1, x2;
+	printf("\nEnter the coefficient of x square: ");
+	scanf("%f",&a);
+	printf("\nEnter the coefficient of x: ");
+	scanf("%f",&b);
+	printf("\nEnter the constant: ");
+	scanf("%f",&c);
+	if(a==0)
+	{
+		printf("\nInvalid input for a");
+	}
+	x1 = (-b+sqrt(b*b-4*a*c))/2*a;
+	x2 = (-b-sqrt(b*b-4*a*c))/2*a;
+	printf("\n The two roots of the quadratic equation are %f and %f", x1,x2);
+	if((b*b-4*a*c)==0)
+	{
+		printf("\nThe roots of the equation are equal and real");
+	}
+	else if((b*b-4*a*c)>=0)
+			{
+				
+				printf("\nThe roots of the equation are unequal and real");
+	
+			}
+			else
+			{
+				printf("\nThe roots of the equation are imaginary");
+			}
+}  // end main
