13ba0b206bcc7c6eb386783dc3cc64f0aef849fc
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-09-20 07:08:30 -0800

    Create ques05.c

---
+++ Labs/L05/ques05.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+    char oper;
+
+    // Input the first number
+    printf("Enter the first number: ");
+    scanf("%f", &num1);
+
+    // Input the arithmetic operator
+    printf("Enter an arithmetic operator (+, -, *, /): ");
+    scanf("%c", &oper); // Note the space before %c to consume any newline character
+
+    // Input the second number
+    printf("Enter the second number: ");
+    scanf("%f", &num2);
+
+    // Perform the requested operation based on the operator
+    switch (oper) {
+        case '+':
+            printf("%f + %f = %f\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%f - %f = %f\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%f * %f = %f\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%f / %f = %f\n", num1, num2, num1 / num2);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator entered.\n");
+    }
+
+    return 0;
+}		// end main()
