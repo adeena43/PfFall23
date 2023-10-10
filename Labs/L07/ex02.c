3de332bfbdab082bd5b33be45ea1c09b4d29c631
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-10-10 08:58:01 +0500

    Create ex02.c

---
+++ Labs/L07/ex02.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the size of the array: ");
+	scanf("%d",&n);
+	int arr[n];
+	int i;
+	int j, k;
+	int pos;
+	int temp;
+	for(i=0;i<n;i++)
+	{
+		printf("\nEnter the element number %d: ",i+1);
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d, ",arr[i]);
+	
+	}
+	
+	printf("\nEnter the position by which you want to rotate the array: ");
+	scanf("%d",&pos);
+	printf("\n");
+	
+	for(j=0;j<pos;j++)
+	{
+		temp = arr[0];
+		for( k =0;k<n-1;k++)
+		{
+			arr[k]=arr[k+1];
+		}
+		arr[n-1]=temp;
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		printf("%d, ",arr[i]);
+	
+	}
+}
