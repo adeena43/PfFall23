b8fb6687d07986fb194e5d128071775a39fbe81d
adeena43 <142868138+adeena43@users.noreply.github.com>
2023-11-28 10:08:54 +0500

    Update ques02.c

--- Labs/L12/ques02.c
+++ Labs/L12/ques02.c
@@ -8,49 +8,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
-
-int compareFiles(FILE *file1, FILE *file2) {
+int compare(FILE *fptr1, FILE *fptr2) {
     char ch1, ch2;
 
-    while ((ch1 = fgetc(file1)) != EOF && (ch2 = fgetc(file2)) != EOF) {
-        if (ch1 != ch2) {
-            return 0; // Files are not equal
-        }
-    }
-
-    // Check if both files reached the end
-    if (ch1 != EOF || ch2 != EOF) {
-        return 0; // Files have different lengths
-        
-    }
+    while ((ch1 = fgetc(fptr1)) != EOF && (ch2 = fgetc(fptr2)) != EOF) 
+	{
+        if (ch1 != ch2) 
+		{
+            return 0; // files are not equal
+        }// end if
+       if (ch1 != EOF || ch2 != EOF) 
+ 	   {
+        return 0; // files are not equal wrt length  
+       }// end if
 	
     return 1; // Files are equal
-}
+   }// end while
+}// end compare
+int main()
+{
 
-int main() {
-    FILE *file1, *file2;
-
-    // Open the files in binary mode
-    file1 = fopen("file1.txt", "rb");
-    file2 = fopen("file2.txt", "rb");
-
-    if (file1 == NULL || file2 == NULL) {
-        perror("Error opening files");
+FILE* fptr1;
+FILE* fptr2;
+fptr1= fopen("file1.txt", "w");
+fptr2=fopen("file2.txt","w");
+fprintf(fptr1,"%s %s %s %s","this","is","a","test");
+fprintf(fptr2,"%s %s %s %s","this","is","a","test");
+fptr1= fopen("file1.txt", "rb");
+fptr2=fopen("file2.txt","rb");
+    if (fptr1 == NULL || fptr2 == NULL) 
+	{
+        perror("Error opening files\n");
         return 1;
-    }
-
-    // Compare files
-    if (compareFiles(file1, file2)) {
+    }//end if
+    if (compare(fptr1, fptr2)) 
+	{
         printf("The files are equal.\n");
-    } else {
+    }// end if 
+	else 
+	{
         printf("The files are not equal.\n");
-    }
-
-    // Close the files
-    fclose(file1);
-    fclose(file2);
+    }// end else
+    fclose(fptr1);
+    fclose(fptr2);
 
     return 0;
 }
-
